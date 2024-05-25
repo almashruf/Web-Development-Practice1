d96a7c0fa1c8106530ca3b8ab479bb516ac448e4
Al Mashruf Tonoy <65592124+almashruf@users.noreply.github.com>
2024-05-25 10:29:04 +0600

    Add files via upload
    react header and footer component practice

--- App.jsx
+++ App.jsx
@@ -1,11 +1,7 @@
-import Clock from './components/Clock';
 
-function App(){
-  return App(
-    <div>
-      <Clock/>
-    </div>
-  );
+
+function App() {
+
 }
 
-export default App;
+export default App
---
+++ Header.jsx
@@ -0,0 +1,21 @@
+
+
+function Header(){
+    return(
+        <header>
+            <h1>My website</h1>
+            <nav>
+                <ul>
+                    <li><a href="#">Home</a></li>
+                    <li>About</li>
+                    <li>Contact</li>
+                    
+                </ul>
+            </nav>
+
+            <hr />
+        </header>
+    )
+}
+
+export default Header
